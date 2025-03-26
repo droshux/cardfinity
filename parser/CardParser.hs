ff6da61ef98ba54b5c732c657e3b9a0913c820be
Hux <droshux@gmail.com>
2025-03-26 20:21:19 +0000

    Move common parsing functions to separate file.
    To prevent cyclic dependencies.

--- parser/CardParser.hs
+++ parser/CardParser.hs
@@ -1,39 +1,15 @@
-module Parser
-  ( CardParser,
-    card,
-    space,
-    hspace,
-    name,
-    gap,
-  )
-where
+module CardParser ( card ) where
 
 import Atoms (discardDeck, drawEffect)
 import Control.Applicative ((<|>))
 import Control.Monad (void)
 import Data.Functor (($>))
 import Data.Set.Ordered (OSet, empty, fromList)
-import Data.Void (Void)
-import Text.Megaparsec (Parsec, choice, manyTill, option, optional, sepBy, sepBy1, someTill, (<?>))
+import ParserCore
+import Text.Megaparsec (choice, manyTill, option, optional, sepBy, sepBy1)
 import Text.Megaparsec.Char (char, string')
-import Text.Megaparsec.Char qualified as Mega
-import Text.Megaparsec.Char.Lexer (charLiteral, decimal, nonIndented, skipBlockComment, skipLineComment)
-import Text.Megaparsec.Char.Lexer qualified as MegaLex
+import Text.Megaparsec.Char.Lexer (decimal, nonIndented)
 import Types (Card (..), CardStats (..), Effect, Monster (..), Requirement, Spell (..), Trigger (..))
-
-type CardParser = Parsec Void String
-
-mkspace :: CardParser () -> CardParser ()
-mkspace s = MegaLex.space s (skipLineComment "!") (skipBlockComment "*" "*")
-
-space :: CardParser ()
-space = mkspace Mega.space1
-
-hspace :: CardParser ()
-hspace = mkspace Mega.hspace1
-
-newline :: CardParser ()
-newline = void $ hspace *> (Mega.newline <|> char ';')
 
 card :: CardParser Card
 card = do
@@ -59,12 +35,6 @@ trigger = do
       OnTap <$ string' "tap",
       Infinity <$ string' "infinity"
     ]
-
-gap :: CardParser ()
-gap = void $ char ',' *> hspace
-
-name :: CardParser String
-name = char '"' *> someTill charLiteral (char '"')
 
 families :: CardParser (OSet String)
 families = fromList <$> name `sepBy` gap
---
+++ parser/ParserCore.hs
@@ -0,0 +1,28 @@
+module ParserCore where
+
+import Control.Applicative ((<|>))
+import Control.Monad (void)
+import Data.Void (Void)
+import Text.Megaparsec (Parsec, someTill)
+import Text.Megaparsec.Char qualified as Mega
+import Text.Megaparsec.Char.Lexer qualified as MegaLex
+
+type CardParser = Parsec Void String
+
+mkspace :: CardParser () -> CardParser ()
+mkspace s = MegaLex.space s (MegaLex.skipLineComment "!") (MegaLex.skipBlockComment "*" "*")
+
+space :: CardParser ()
+space = mkspace Mega.space1
+
+hspace :: CardParser ()
+hspace = mkspace Mega.hspace1
+
+newline :: CardParser ()
+newline = void $ hspace *> (Mega.newline <|> Mega.char ';')
+
+gap :: CardParser ()
+gap = void $ Mega.char ',' *> hspace
+
+name :: CardParser String
+name = Mega.char '"' *> someTill MegaLex.charLiteral (Mega.char '"')
