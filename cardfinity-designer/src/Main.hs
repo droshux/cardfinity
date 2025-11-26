af7b3ec2e02a29b5d5366ec7e2ebba83c4683600
Hux <droshux@gmail.com>
2025-11-26 13:47:17 +0000

    clean: undo everything and start again
    I was taking a totally wrong approach

--- cardfinity-designer/src/Main.hs
+++ cardfinity-designer/src/Main.hs
@@ -3,58 +3,9 @@
 
 module Main where
 
-import Atoms (FindCards)
-import Atoms qualified as A
-import Data.Foldable (toList)
-import Data.List.NonEmpty qualified as NE (NonEmpty ((:|)))
-import Data.Set.Ordered
-import Editor.Card (cardEditor)
-import Editor.Monster (monsterEditor)
-import Editor.Shared (noLens)
-import Editor.Spell (spellEditor)
-import Miso (Component (bindings), text, toMisoString)
 import Miso qualified as M
-import Miso.CSS qualified as CSS
-import Miso.Effect (noop)
-import Miso.Html qualified as H
-import Miso.Lens (Lens, lens, (%=), (.=), (^.))
-import Miso.Lens.TH (makeLenses)
-import Miso.Types ((+>), (<-->))
-import Scale (runScale)
-import ShowCard ()
-import Types (Card (..), CardStats (SpellStats), Monster (..), Spell (..), Trigger (OnPlay))
 
 main :: IO ()
 main = M.run (M.startApp app)
 
-newtype MainModel = Model
-  { _monster :: Card
-  }
-  deriving (Eq)
-
-monster = lens _monster $ \m m' -> m {_monster = m'}
-
-app = M.component (Model {_monster = def}) update view
-
-update _ = monster .= def
-
-def = Card {_cardStats = SpellStats (Spell {_spellTrigger = OnPlay, _spellName = "", _effects = [], _castingConditions = empty}), _cardImageUrl = Nothing, _cardID = 0, _cardFamilies = empty}
-
-view :: MainModel -> M.View MainModel ()
-view m =
-  H.div_
-    []
-    [ H.div_
-        []
-        [ text (M.toMisoString $ show $ runScale [] (m ^. monster)),
-          H.button_ [H.onClick ()] [text "Reset"],
-          H.pre_
-            [ CSS.style_
-                [ CSS.border "solid",
-                  CSS.width "fit-content"
-                ]
-            ]
-            [text $ M.toMisoString $ show $ m ^. monster]
-        ],
-      H.div_ [] +> (cardEditor {bindings = [monster <--> noLens]})
-    ]
+app = M.component () M.noop $ const $ M.text "WIP"
